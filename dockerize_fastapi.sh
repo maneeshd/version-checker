32af83b2b3ec0951af521c3a87b7f70cd0751abb
Maneesh Divana <maneeshd77@gmail.com>
2020-02-15 19:17:17 -0600

    Updated dockeriezer scripts

---
+++ dockerize_fastapi.sh
@@ -0,0 +1,20 @@
+set -e
+
+echo ""
+echo "Dockerizing the API Sevrer..."
+echo ""
+echo "Building docker image..."
+echo ""
+docker build --file Dockerfile --compress --force-rm --tag "maneeshd/version_checker:latest" .
+echo ""
+echo "Docker image built"
+echo ""
+echo "Pushing the docker image to DockerHub..."
+echo ""
+docker push maneeshd/version_checker:latest
+echo ""
+echo "Pushed the docker image to DockerHub."
+echo ""
+echo "Running the image..."
+echo ""
+docker run --rm -p 8000:8000 --name version_checker maneeshd/version_checker:latest
--- dockerize_flask.sh
+++ dockerize_flask.sh
@@ -1,18 +1,20 @@
+set -e
+
 echo ""
 echo "Dockerizing the API Sevrer..."
 echo ""
 echo "Building docker image..."
 echo ""
-docker build --compress --force-rm --tag "maneeshd/version-checker:latest" .
+docker build --file Dockerfile_Flask --compress --force-rm --tag "maneeshd/version_checker_flask:latest" .
 echo ""
 echo "Docker image built"
 echo ""
 echo "Pushing the docker image to DockerHub..."
 echo ""
-docker push --disable-content-trust=false maneeshd/version-checker:latest
+docker push maneeshd/version_checker_flask:latest
 echo ""
 echo "Pushed the docker image to DockerHub."
 echo ""
 echo "Running the image..."
 echo ""
-docker run --rm -p 8000:8000 --name version_checker maneeshd/version-checker
+docker run --rm -p 8000:8000 --name version_checker_flask maneeshd/version_checker_flask:latest
